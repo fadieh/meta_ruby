3204eb3a43195a470817b71463ca977a3a3d26fa
Fadie Hannona <fadiehannona@gmail.com>
2014-11-17 10:57:52 +0000

    cleaned up

--- meta.rb
+++ meta.rb
@@ -20,19 +20,4 @@ class Student
   	puts "It's all good, the method #{method_name} doesn't exist"
   end
 
-  define_method(:wilma) { puts "LOLOLOLOL" }
-
 end
-
-class B < Student
-	define_method(:barney, instance_method(:fred))
-end
-
-a = B.new
-a.barney
-a.wilma
-a.create_method(:betty) { p self }
-a.betty
-a.create_method(:has_unixoid?) { puts @badges.include?('unixoid')}
-a.has_unixoid?
-
